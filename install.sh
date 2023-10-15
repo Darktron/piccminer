1eed1f2431cc7417c1dd76a32a6bfaa67ab8973b
Darktron <omyp12@gmail.com>
2023-10-15 04:56:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git scree
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-chmod +x ~/piccminer/oc.sh ~/piccminer/ccminer ~/piccminer/start.sh
-chmod 755 ~/piccminer/mod5b.dtb
-sudo mv ~/piccminer/mod5b.dtb /boot/dtb/rockchip/
-sudo mv ~/piccminer/oc.service /etc/systemd/system/
+cd ~/ccminer
+chmod +x oc.sh ccminer start.sh
+chmod 755 mod5b.dtb
+mv mod5b.dtb /boot/dtb/rockchip/
+mv oc.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start oc.service
 sudo systemctl enable oc.service
