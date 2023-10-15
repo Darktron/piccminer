2a051b23542fd1293c777a594eae11b48db248e0
Darktron <omyp12@gmail.com>
2023-10-15 04:49:15 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ screen -S ccminer -X stuff "~/piccminer/ccminer -c ~/piccminer/config.json\n" 1>
 printf '\nMining started.\n'
 printf '===============\n'
 printf '\nManual:\n'
-printf 'start: ~/.piccminer/start.sh\n'
+printf 'start: ~/piccminer/start.sh\n'
 printf 'stop: screen -X -S ccminer quit\n'
 printf '\nmonitor mining: screen -x ccminer\n'
 printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
