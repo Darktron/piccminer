b88f832af46003a52ea3cb4f42271d1dd59174a0
Darktron <omyp12@gmail.com>
2023-10-15 06:01:02 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo apt-get -y update 
+sudo apt-get -y update
+sudo apt-get install --fix-missing
 sudo apt-get -y upgrade
 sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
