552cc5a4a4812d5b745c31bb704a27f1ec5143f4
Darktron <omyp12@gmail.com>
2023-10-15 08:53:19 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,7 @@ sudo apt install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano 
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-git clone https://github.com/Darktron/piccminer.git
-cd ~/piccminer
-chmod +x oc.sh ccminer start.sh install.sh
+chmod +x oc.sh ccminer start.sh
 chmod 755 mod5b.dtb
 sudo chown root:root mod5b.dtb
 sudo mv mod5b.dtb /boot/dtb/rockchip/
