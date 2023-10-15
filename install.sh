c9eb7c6f72beb95fa93a7cbad4e361194c315a6b
Darktron <omyp12@gmail.com>
2023-10-15 05:33:54 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,6 @@ sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git scree
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-
-if [ ! -d ~/piccminer ]
-then
-  mkdir ~/piccminer
-fi
-cd ~/piccminer
-
 git clone https://github.com/Darktron/piccminer.git
 chmod +x oc.sh ccminer start.sh install.sh
 chmod 755 mod5b.dtb
